00a4909b047bbb8340a102684b3b4a6234d8a11f
Garrett <zerocool@Garretts-MacBook-Pro.local>
2017-06-16 08:46:16 -0500

    done

--- public/profileCtrl.js
+++ public/profileCtrl.js
@@ -1,4 +1,10 @@
 angular.module('userProfiles')
 .controller('profileCtrl', function( $scope ) {
+
+	friendsService.getFriends().then(function(response){
+		$scope.currentUser = response.date.currentUser;
+		$scope.friends = response.data.friends
+	})
+
 	// FIX ME - assign values to $scope.currentUser and $scope.friends
 });
